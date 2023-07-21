a2f462368a9ef2c77bf0367b8d7bd7f9a91a45c5
Matt McKeever <mmckeever23@gmail.com>
2023-07-21 15:33:34 -0500

    customtk buttons

--- Rolling America.py
+++ Rolling America.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import ttkbootstrap as ttk
+import customtkinter as ctk
 from ttkbootstrap.constants import *
 
 root = tk.Tk()
@@ -12,6 +13,7 @@ x = int((screen_width / 2) - (app_width / 2))
 y = int((screen_height / 2) - (app_height / 2))
 root.geometry(f'{screen_width}x{screen_height}')
 
+
 board_path = 'board.png'
 board_image = tk.PhotoImage(file=board_path)
 
@@ -21,10 +23,10 @@ board.place(x=f'{x}', y=f'{y}')
 title = ttk.Label(root, text="Rolling America", font=('Magneto', 50))
 title.pack()
 
-b1 = ttk.Button(root, text="Button 1", bootstyle=SUCCESS)
+b1 = ctk.CTkButton(root, text="Button 1")
 b1.pack(padx=5, pady=10)
 
-b2 = ttk.Button(root, text="Button 2", bootstyle=(INFO, OUTLINE))
+b2 = ctk.CTkButton(root, text="Button 2")
 b2.pack(padx=5, pady=10)
 
 root.mainloop()
