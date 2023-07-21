40257e023d4aef9c9dae6346deea743bbebebe08
Matt McKeever <mmckeever23@gmail.com>
2023-07-21 14:25:16 -0500

    board created

--- Rolling America.py
+++ Rolling America.py
@@ -1,12 +1,30 @@
-import customtkinter
+import tkinter as tk
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
 
-def button_callback():
-    print("button clicked")
+root = tk.Tk()
+root.title("Rolling America")
+app_width = 1484
+app_height = 864
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x = int((screen_width / 2) - (app_width / 2))
+y = int((screen_height / 2) - (app_height / 2))
+root.geometry(f'{screen_width}x{screen_height}')
 
-app = customtkinter.CTk()
-app.geometry("400x150")
+board_path = 'board.png'
+board_image = tk.PhotoImage(file=board_path)
 
-button = customtkinter.CTkButton(app, text="my button", command=button_callback)
-button.pack(padx=20, pady=20)
+board = tk.Label(root, image=board_image)
+board.place(x=f'{x}', y=f'{y}')
 
-app.mainloop()
+title = ttk.Label(root, text="Rolling America", font=('Magneto', 50))
+title.pack()
+
+b1 = ttk.Button(root, text="Button 1", bootstyle=SUCCESS)
+b1.pack(padx=5, pady=10)
+
+b2 = ttk.Button(root, text="Button 2", bootstyle=(INFO, OUTLINE))
+b2.pack(padx=5, pady=10)
+
+root.mainloop()
