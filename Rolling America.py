7851f4baa04a985dcfa32f6cfe9e3d2dcb7e8939
Matt McKeever <mmckeever23@gmail.com>
2023-07-22 10:25:16 -0500

    added first combobox

--- Rolling America.py
+++ Rolling America.py
@@ -13,20 +13,24 @@ x = int((screen_width / 2) - (app_width / 2))
 y = int((screen_height / 2) - (app_height / 2))
 root.geometry(f'{screen_width}x{screen_height}')
 
-
+# Board and title elements
 board_path = 'board.png'
 board_image = tk.PhotoImage(file=board_path)
-
 board = tk.Label(root, image=board_image)
 board.place(x=f'{x}', y=f'{y}')
-
 title = ttk.Label(root, text="Rolling America", font=('Magneto', 50))
 title.pack()
 
-b1 = ctk.CTkButton(root, text="Button 1")
-b1.pack(padx=5, pady=10)
+# Combobox choices
+combo1_choice = ctk.StringVar()
 
-b2 = ctk.CTkButton(root, text="Button 2")
-b2.pack(padx=5, pady=10)
+dice_values = [1, 2, 3, 4, 5, 6]
+
+combo1 = ttk.Combobox(root, values=dice_values, width=2, justify="center", textvariable=combo1_choice, state="readonly")
+
+combo1.place(x=267, y=165)
+
+b1 = ctk.CTkButton(root, text="New Game")
+b1.pack(padx=5, pady=10)
 
 root.mainloop()
